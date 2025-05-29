0d54d16700cb7daa0e1085d32e04f6237289a799
NUNO MIGUEL RIBEIRO FERREIRA <al76889@alunos.utad.pt>
2025-05-29 17:45:23 +0100

    Add project files.

---
+++ FinanceiroServiceWCF/FinanceiroService.svc.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace FinanceiroServiceWCF
+{
+    public class FinanceiroService : IFinanceiroService
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["ContabilidadeDB"].ConnectionString;
+
+        public string GetPecaMaiorPrejuizo()
+        {
+            string codigoPeca = "Nenhuma";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = @"
+                    SELECT TOP 1 Codigo_Peca
+                    FROM Custos_Peca
+                    GROUP BY Codigo_Peca
+                    ORDER BY SUM(Prejuizo) DESC";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+
+                var result = cmd.ExecuteScalar();
+
+                if (result != null)
+                    codigoPeca = result.ToString();
+            }
+
+            return codigoPeca;
+        }
+
+        public decimal GetCustoTotalProducao(DateTime dataInicio, DateTime dataFim)
+        {
+            decimal total = 0;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = @"
+            SELECT SUM(cp.Custo_Producao)
+            FROM Contabilidade.dbo.Custos_Peca cp
+            JOIN Producao.dbo.Produto p ON cp.ID_Produto = p.ID_Produto
+            WHERE p.Data_Producao BETWEEN @DataInicio AND @DataFim";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@DataInicio", dataInicio);
+                cmd.Parameters.AddWithValue("@DataFim", dataFim);
+
+                conn.Open();
+                var result = cmd.ExecuteScalar();
+
+                if (result != DBNull.Value && result != null)
+                    total = Convert.ToDecimal(result);
+            }
+
+            return total;
+        }
+
+        public decimal GetLucroTotal(DateTime dataInicio, DateTime dataFim)
+        {
+            decimal total = 0;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = @"
+            SELECT SUM(cp.Lucro)
+            FROM Contabilidade.dbo.Custos_Peca cp
+            JOIN Producao.dbo.Produto p ON cp.ID_Produto = p.ID_Produto
+            WHERE p.Data_Producao BETWEEN @DataInicio AND @DataFim";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@DataInicio", dataInicio);
+                cmd.Parameters.AddWithValue("@DataFim", dataFim);
+
+                conn.Open();
+                var result = cmd.ExecuteScalar();
+
+                if (result != DBNull.Value && result != null)
+                    total = Convert.ToDecimal(result);
+            }
+
+            return total;
+        }
+
+        public List<PrejuizoPorPeca> GetPrejuizoTotalPorPeca(DateTime dataInicio, DateTime dataFim)
+        {
+            List<PrejuizoPorPeca> lista = new List<PrejuizoPorPeca>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = @"
+            SELECT cp.Codigo_Peca, SUM(cp.Prejuizo) AS TotalPrejuizo
+            FROM Contabilidade.dbo.Custos_Peca cp
+            JOIN Producao.dbo.Produto p ON cp.ID_Produto = p.ID_Produto
+            WHERE p.Data_Producao BETWEEN @DataInicio AND @DataFim
+            GROUP BY cp.Codigo_Peca";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@DataInicio", dataInicio);
+                cmd.Parameters.AddWithValue("@DataFim", dataFim);
+
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    lista.Add(new PrejuizoPorPeca
+                    {
+                        CodigoPeca = reader["Codigo_Peca"].ToString(),
+                        TotalPrejuizo = Convert.ToDecimal(reader["TotalPrejuizo"])
+                    });
+                }
+            }
+
+            return lista;
+        }
+
+        public DadosFinanceiros GetDadosFinanceirosPorPeca(string codigoPeca)
+        {
+            DadosFinanceiros dados = null;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = @"
+            SELECT 
+                Codigo_Peca,
+                SUM(Custo_Producao) AS Custo,
+                SUM(Lucro) AS Lucro,
+                SUM(Prejuizo) AS Prejuizo,
+                SUM(Tempo_Producao) AS TempoTotal
+            FROM Contabilidade.dbo.Custos_Peca
+            WHERE Codigo_Peca = @Codigo
+            GROUP BY Codigo_Peca";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Codigo", codigoPeca);
+
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    dados = new DadosFinanceiros
+                    {
+                        CodigoPeca = reader["Codigo_Peca"].ToString(),
+                        Custo = Convert.ToDecimal(reader["Custo"]),
+                        Lucro = Convert.ToDecimal(reader["Lucro"]),
+                        Prejuizo = Convert.ToDecimal(reader["Prejuizo"]),
+                        TempoProducao = Convert.ToInt32(reader["TempoTotal"])
+                    };
+                }
+            }
+
+            return dados;
+        }
+
+    }
+}
---
+++ FinanceiroServiceWCF/IFinanceiroService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace FinanceiroServiceWCF
+{
+    [ServiceContract]
+    public interface IFinanceiroService
+    {
+        [OperationContract]
+        string GetPecaMaiorPrejuizo();
+
+        [OperationContract]
+        decimal GetCustoTotalProducao(DateTime dataInicio, DateTime dataFim);
+
+        [OperationContract]
+        decimal GetLucroTotal(DateTime dataInicio, DateTime dataFim);
+
+        [OperationContract]
+        List<PrejuizoPorPeca> GetPrejuizoTotalPorPeca(DateTime dataInicio, DateTime dataFim);
+
+        [OperationContract]
+        DadosFinanceiros GetDadosFinanceirosPorPeca(string codigoPeca);
+    }
+
+    // Classe auxiliar para prejuízo total por peça
+    [DataContract]
+    public class PrejuizoPorPeca
+    {
+        [DataMember]
+        public string CodigoPeca { get; set; }
+
+        [DataMember]
+        public decimal TotalPrejuizo { get; set; }
+    }
+
+    // Classe auxiliar para dados financeiros detalhados
+    [DataContract]
+    public class DadosFinanceiros
+    {
+        [DataMember]
+        public string CodigoPeca { get; set; }
+
+        [DataMember]
+        public decimal Custo { get; set; }
+
+        [DataMember]
+        public decimal Lucro { get; set; }
+
+        [DataMember]
+        public decimal Prejuizo { get; set; }
+
+        [DataMember]
+        public int TempoProducao { get; set; }
+    }
+}
